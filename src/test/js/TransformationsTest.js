b6c3e9b4bee03ecc440002abab489d0945f20fa1
root <root@localhost>
2016-06-15 09:41:37 +1000

    Fix logger in unit test

--- src/test/js/TransformationsTest.js
+++ src/test/js/TransformationsTest.js
@@ -1,9 +1,8 @@
 var currentWorkingDir = java.lang.System.getProperty("user.dir");
 var frameworkLocation = currentWorkingDir + '/target/framework/js/';
 var configLocation = currentWorkingDir + '/src/main/js/';
-
 var Require = load('src/main/js/lib/Require.js');
-var require = Require('./', [ configLocation, frameworkLocation ]);
+var require = Require( './' , [ frameworkLocation, configLocation ] );
 
 describe(
 		"TransformationsTest",
@@ -13,10 +12,7 @@ describe(
 			var console;
 			var apim;
 
-			var logger = require('Logger.js').newLogger({ 
-					name: "gatewayscript-user",
-					logLevel: "7"
-				}, console);
+			var logger;
 			var config = [ {
 				name : "/users",
 				methods : [ {
@@ -37,10 +33,14 @@ describe(
 						'emergency', 'options' ]);
 				apim = jasmine.createSpyObj('apim', [ 'getvariable' ]);
 
+				logger = require('Logger.js').newLogger({ 
+					logLevel: "7"
+				}, console);
+			
 				var log = function(msg) {
 					print(msg);
 				}
-
+				
 				console.info.and.callFake(log);
 				console.notice.and.callFake(log);
 				console.debug.and.callFake(log);
@@ -54,6 +54,7 @@ describe(
 					var api = require("Api.js").newApi(frameworkLocation,
 							"api", "1.0.0", config,
 							logger, logger);
+					api.setConfigLocation(configLocation);
 
 					// mock body to transform
 					var body = {
@@ -95,6 +96,7 @@ describe(
 							var api = require("Api.js").newApi(
 									frameworkLocation, "api", "1.0.0", config,
 									logger, logger);
+							api.setConfigLocation(configLocation);
 
 							// mock body to transform
 							var body = {
